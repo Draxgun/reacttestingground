3e8c4dc536bf60ab5eb3d480d2b8285a6f8d7abd
Jesus Alejandro Bañuelos Mendoza <76793906+Draxgun@users.noreply.github.com>
2023-12-05 21:05:25 -0600

    Practice React Query

--- src/App.js
+++ src/App.js
@@ -5,26 +5,38 @@ import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
 import { Contact } from './pages/Contact';
 import { Navbar } from './Navbar';
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 
 export const AppContext = createContext();
 
 function App() {
+
+  const client = new QueryClient({
+    defaultOptions: {
+      
+    }
+  });
+  
+
   let [username,setUsername] = useState("Alejandro");
 
   return (
     <div className="App">
-      <AppContext.Provider value={{username,setUsername}}>
-        <Router>
-          <Navbar/>
 
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            <Route path="*" element={<h1>404 Page Not Found</h1>}/>
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+        <AppContext.Provider value={{username,setUsername}}>
+          <QueryClientProvider client={client}>
+          <Router>
+            <Navbar/>
+
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+              <Route path="/contact" element={<Contact/>}/>
+              <Route path="*" element={<h1>404 Page Not Found</h1>}/>
+            </Routes>
+          </Router>
+          </QueryClientProvider>
+        </AppContext.Provider>
 
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,28 @@
-import { useContext } from "react";
-import { AppContext } from "../App";
+import {useQuery} from "@tanstack/react-query"
+import Axios from "axios"
 
 export const Home = () => {
-    let {username} =  useContext(AppContext);
+    
+    const {data: catData, isLoading, isError, refetch} = useQuery({
+            queryKey: ["cat"],
+            queryFn: () => {
+                return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+            }
+        });  
+    
+    if(isLoading){
+        return <h1>Loading..</h1>
+    }
+
+    if(isError){
+        return <h1>Something went wrong oops</h1>
+    }
+
     return (
     <div>
-        This is a homepage and the user is {username}
+        <p>{catData?.fact}</p>
+        <button onClick={refetch}> Update data</button>
+
     </div>
     );
 };
